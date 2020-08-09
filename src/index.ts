e9551d4df0764f64da4ae3acffa366d7082c581a
Roman Zinchuk <zigzag2341@gmail.com>
2020-08-09 04:48:59 +0300

    pages links autogeneration added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello');
+import {Routing} from '../routing';
+import $ from "jquery";
+import './index.scss';
+
+window.onload = () => {
+  let body = $('body');
+  let links = Routing.pages.forEach(page => {
+      let element: JQuery<HTMLAnchorElement> = $('<a>');
+      element.attr('href',`pages/${page.name}/${page.name}.html`);
+      element.text(page.name);
+      element.addClass('nav-link');
+      body.append(element);
+  });
+}
